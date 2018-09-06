ec73386b25df984f97bb1bba6751279f5df1ca30
chrisesquerra <esquerra>
2018-09-05 23:01:17 -0500

    still working on functions.  Looks pretty, though.

--- simon.js
+++ simon.js
@@ -1,18 +1,43 @@
-var colorArray = ["green, red, yellow, blue"];
+var colorArray = ["green", "red", "yellow", "blue"];
 var gameArray;
 var playerArray;
-var count;
+var count = 0;
 
 
-
-function gameMoves(){
+$("#startButton").click(function () {
     gameArray = [];
-    count = 0;
-    for (var i = -1, i<count;i++){
-        gameArray.push(Math.floor(Math.random() * colorsArray.length));
+    for (var i = -1; i < count; i++) {
+        console.log("entered first loop");
+        gameArray.push(colorArray[Math.floor(Math.random() * 4)]);
     }
-}   return gameArray;
+    console.log("passing array " +gameArray+ " to second loop");
+    displayMoves(gameArray);
+});
 
-function displayMoves (){
 
+function displayMoves(array) {
+    for (var i = 0; i < array.length; i++) {
+        console.log("entering second loop");
+        setInterval(function () {
+            $("#" + array[i]+"").addClass("active");
+            console.log("activated light");
+            setTimeout(function () {
+                $("#" + array[i]+"").removeClass("active");
+                console.log("removed light");
+            }, 500);
+        }, 500);
+    }
 }
+
+
+$(".gamecell").click(function () {
+    $(this).addClass("active");
+    setTimeout(function () {
+        $(this).removeClass("active");
+    }, 250)
+});
+
+
+
+
+
