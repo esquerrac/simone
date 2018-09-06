d6f574812dfd3f6f007748a8531d72d740cf5415
chrisesquerra <esquerra>
2018-09-05 20:49:30 -0500

    Initial commit of simple simon game in progress. styling complete, building game functions

---
+++ simon.js
@@ -0,0 +1,18 @@
+var colorArray = ["green, red, yellow, blue"];
+var gameArray;
+var playerArray;
+var count;
+
+
+
+function gameMoves(){
+    gameArray = [];
+    count = 0;
+    for (var i = -1, i<count;i++){
+        gameArray.push(Math.floor(Math.random() * colorsArray.length));
+    }
+}   return gameArray;
+
+function displayMoves (){
+
+}
