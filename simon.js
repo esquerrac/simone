4edec37c1ed27f9cf195d70a62858b6263a35d7a
chrisesquerra <esquerra>
2018-09-19 20:52:25 -0500

    most functionality and initial styling finished.  still working on light functions during random sequence display.  Timer display needs adjustment.

--- simon.js
+++ simon.js
@@ -1,41 +1,115 @@
-var colorArray = ["green", "red", "yellow", "blue"];
-var gameArray;
-var playerArray;
+var gameArray = [];
+let colorsArray = ["green", "red", "yellow", "blue"];
+var playerArray = [];
 var count = 0;
 
-
+function mouseDown(event){
+    $(this).addClass("active");
+};
+function mouseUp(event){
+    $(this).removeClass("active");
+};
 $("#startButton").click(function () {
-    gameArray = [];
-    for (var i = -1; i < count; i++) {
-        console.log("entered first loop");
-        gameArray.push(colorArray[Math.floor(Math.random() * 4)]);
-    }
-    console.log("passing array " +gameArray+ " to second loop");
-    displayMoves(gameArray);
+    createMoves(gameArray);
+    $("#counter").text("Score: " + count);
+    $("#status").text("Watch Closely...");
 });
 
+function createMoves(array) {
+    for (let i = 0; i <= count; i++) {
+        array.push(colorsArray[(Math.floor(Math.random() * colorsArray.length))]);
+        displayMoves(gameArray);
+    }
+}
 
 function displayMoves(array) {
-    for (var i = 0; i < array.length; i++) {
-        console.log("entering second loop");
-        setInterval(function () {
-            $("#" + array[i]+"").addClass("active");
-            console.log("activated light");
+    for (let i = 0; i < array.length; i++) {
+        var currentLight = array[i].toString();
+        if (currentLight === "green") {
+            $("#green").addClass("active");
             setTimeout(function () {
-                $("#" + array[i]+"").removeClass("active");
-                console.log("removed light");
+                $("#green").removeClass("active");
             }, 500);
-        }, 500);
+        } else if (currentLight === "red") {
+            $("#red").addClass("active");
+            setTimeout(function () {
+                $("#red").removeClass("active");
+            }, 500);
+        } else if (currentLight === "yellow") {
+            $("#yellow").addClass("active");
+            setTimeout(function () {
+                $("#yellow").removeClass("active");
+            }, 500);
+        } else if (currentLight === "blue") {
+            $("#blue").addClass("active");
+            setTimeout(function () {
+                $("#blue").removeClass("active");
+            }, 500);
+        }
+    }
+    setTimeout(function () {
+        playerTurn();
+    }, 500)
+
+}
+
+function playerTurn(event) {
+    // $("#green").css("background-color", "darkgreen");
+    // $("#red").css("background-color", "darkred");
+    // $("#yellow").css("background-color", "darkgoldenrod");
+    // $("#blue").css("background-color", "darkblue");
+    playerArray = [];
+    $(".gamecell").click(function () {
+        playerArray.push($(this).attr("id"));
+    });
+    let timer = 10;
+    var countdown = setInterval(function () {
+        timer--;
+        $("#centerText").text(timer);
+        if (timer === 0) {
+            clearInterval(countdown);
+            $("#centerText").text();
+            checkMoves(playerArray, gameArray);
+        }
+    }, 1000);
+    $("#center").click(function () {
+        clearInterval(countdown);
+        $("#centerText").text();
+        checkMoves(playerArray, gameArray);
+    });
+}
+
+
+function checkMoves(array1, array2) {
+    if (array1.length !== array2.length) {
+        $("#status").text("Oh no!  Better luck next time!");
+        resetGame();
+    } else {
+        for (let i = 0; i < array2.length; i++) {
+            if (array1[i] !== array2[i]) {
+                $("#status").text("Oh no!  Better luck next time!");
+                resetGame();
+            } else {
+                $("#status").text("Great Job!  Get ready for the next round!");
+                count++;
+                $("#counter").text("Score: " + count);
+                playerArray = [];
+                setTimeout(function () {
+                    createMoves(gameArray)
+                }, 1000)
+            }
+        }
     }
 }
 
 
-$(".gamecell").click(function () {
-    $(this).addClass("active");
-    setTimeout(function () {
-        $(this).removeClass("active");
-    }, 250)
-});
+var resetGame = function () {
+    count = 0;
+    gameArray = [];
+    playerArray = [];
+    $("#counter").text("Score: " + count);
+};
+
 
 
 
