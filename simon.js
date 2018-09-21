5f4673d6685e21cfb8acb0a7b58383cbdd79a64f
chrisesquerra <esquerra>
2018-09-20 22:40:12 -0500

    repaired issue with repeated gamestate creation.  refactored loops to ease troubleshooting.  button clicks still increase number of events fired each turn.

--- simon.js
+++ simon.js
@@ -3,129 +3,133 @@ let colorsArray = ["green", "red", "yellow", "blue"];
 var playerArray = [];
 var count = 0;
 
-function mouseDown() {
-    $(this).addClass("active");
-    console.log("button lighting up!")
-};
-
-function mouseUp() {
-    $(this).removeClass("active");
-    console.log("button turning off!")
-};
 $("#startButton").click(function () {
-    console.log("start button clicked")
     $("#counter").text("Score: " + count);
-    console.log("score set")
     $("#status").text("Watch Closely...");
     createMoves(gameArray);
 });
 
 function createMoves(array) {
-    console.log("creating moves array");
-    for (let i = 0; i <= count; i++) {
-        array.push(colorsArray[(Math.floor(Math.random() * colorsArray.length))]);
-        console.log("moves array complete");
-        console.log(gameArray);
-        displayMoves(gameArray);
+    array.push(colorsArray[(Math.floor(Math.random() * colorsArray.length))]);
+    if (gameArray[count] == gameArray[count - 1]) {
+        gameArray.pop()
+        createMoves(gameArray);
     }
+    displayMoves(gameArray);
+    console.log(gameArray);
 }
 
 function displayMoves(array) {
-    let i = 0;
+    for (let i = 0; i < array.length; i++) {
+        lightUp(array[i]);
+        setTimeout(function () {
+            console.log("preparing to activate light")
+        }, 250)
+    }
     setTimeout(function () {
-        var currentLight = array[i].toString();
-        console.log("will activate light:")
-        console.log(currentLight);
-        if (currentLight === "green") {
-            $("#green").addClass("active");
-            setTimeout(function () {
-                $("#green").removeClass("active");
-            }, 500);
-        } else if (currentLight === "red") {
-            $("#red").addClass("active");
-            setTimeout(function () {
-                $("#red").removeClass("active");
-            }, 500);
-        } else if (currentLight === "yellow") {
-            $("#yellow").addClass("active");
-            setTimeout(function () {
-                $("#yellow").removeClass("active");
-            }, 500);
-        } else if (currentLight === "blue") {
-            $("#blue").addClass("active");
-            setTimeout(function () {
-                $("#blue").removeClass("active");
-            }, 500);
-        }
-        console.log("successfully activated lights")
-    }, 500);
-    setTimeout(function () {
-        console.log("preparing to start player turn");
-
         playerTurn();
-        $("#status").text("Now it's your turn!");
-    }, 1000)
+    }, 500);
+}
+
+function lightUp(color) {
+    if (color == "green") {
+        $("#green").addClass("active");
+        setTimeout(function () {
+            $("#green").removeClass("active");
+        }, 500);
+    } else if (color == "red") {
+        $("#red").addClass("active");
+        setTimeout(function () {
+            $("#red").removeClass("active");
+        }, 300);
+    } else if (color == "yellow") {
+        $("#yellow").addClass("active");
+        setTimeout(function () {
+            $("#yellow").removeClass("active");
+        }, 300);
+    } else if (color == "blue") {
+        $("#blue").addClass("active");
+        setTimeout(function () {
+            $("#blue").removeClass("active");
+        }, 300);
+    }
 }
 
 function playerTurn() {
     playerArray = [];
-    console.log("player array cleared");
-    $(".gamecell").click(function () {
-        playerArray.push($(this).attr("id"));
-        console.log("pushed button to player array")
-        console.log(playerArray);
-    });
-    let timer = 10;
-    var countdown = setInterval(function () {
-        timer--;
-        $("#centerText").text(timer);
-        if (timer === 0) {
-            clearInterval(countdown);
-            $("#centerText").text();
+    $("#status").text("Now it's your turn!");
+    $("#green").click(function () {
+        playerArray.push("green");
+        console.log("pushed green to player array");
+        if (playerArray.length === gameArray.length) {
             checkMoves(playerArray, gameArray);
         }
-    }, 1000);
-    $("#center").click(function () {
-        console.log("center button activated")
-        clearInterval(countdown);
-        $("#centerText").text();
-        checkMoves(playerArray, gameArray);
+    });
+    $("#red").click(function () {
+        playerArray.push("red");
+        console.log("pushed red to player array");
+        if (playerArray.length === gameArray.length) {
+            checkMoves(playerArray, gameArray);
+        }
+    });
+    $("#yellow").click(function () {
+        playerArray.push("yellow");
+        console.log("pushed yellow to player array");
+        if (playerArray.length === gameArray.length) {
+            checkMoves(playerArray, gameArray);
+        }
+    });
+    $("#blue").click(function () {
+        playerArray.push("blue");
+        console.log("pushed blue to player array");
+        if (playerArray.length === gameArray.length) {
+            checkMoves(playerArray, gameArray);
+        }
     });
 }
 
 
 function checkMoves(array1, array2) {
-    console.log("preparing to check moves...")
-    if (array1.length !== array2.length) {
-        $("#status").text("Oh no!  Better luck next time!");
-        resetGame();
-    } else {
-        for (let i = 0; i < array2.length; i++) {
-            if (array1[i] !== array2[i]) {
-                $("#status").text("Oh no!  Better luck next time!");
-                resetGame();
-            } else {
+    $("#status").text("Checking your sequence...");
+    console.log("preparing to check moves...");
+    console.log(playerArray);
+    console.log(gameArray);
+    var i = 0;
+    var checkCount = 0;
+    while (i < gameArray.length) {
+        if (array1[i] !== array2[i]) {
+            $("#status").text("Oh no!  Better luck next time!");
+            endGame();
+            break;
+        } else {
+            i++;
+            checkCount++;
+            continue;
+            if (checkCount === gameArray.length) {
                 $("#status").text("Great Job!  Get ready for the next round!");
-                count++;
-                $("#counter").text("Score: " + count);
-                console.log("Success!  score updated")
-                playerArray = [];
-                setTimeout(function () {
+                setTimeout(function() {
+                    count++;
+                    $("#counter").text("Score: " + count);
+                    console.log("Success!  score updated");
                     console.log("re-creating moves array");
-                    createMoves(gameArray)
-                }, 1000)
+                },1000);
             }
         }
     }
+    createMoves(gameArray)
 }
 
 
-var resetGame = function () {
+var endGame = function () {
     count = 0;
     gameArray = [];
     playerArray = [];
     $("#counter").text("Score: " + count);
+    setTimeout(function () {
+        $("#status").text("Press Start to play again!");
+    }, 500);
 };
+
 
 
 
