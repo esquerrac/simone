46d39f8ae719b9f988123333c64571e63b05f435
chrisesquerra <esquerra>
2018-09-22 22:02:52 -0500

    refactored JS with test build.  Will attempt to isolate click event issue from previous build and merge functioning portions of builds.

--- simon.js
+++ simon.js
@@ -1,145 +1,101 @@
-var gameArray = [];
-let colorsArray = ["green", "red", "yellow", "blue"];
 var playerArray = [];
+var gameArray = [];
 var count = 0;
+var colorsArray = ["green", "red", "yellow", "blue"];
+$("#counter").text("Score : "+ count);
+
+
 
 $("#startButton").click(function () {
-    $("#counter").text("Score: " + count);
-    console.log("start button pressed");
-    createMoves(gameArray);
+    $("#status").html("Are you ready?");
+    console.log("Starting game");
+    createMoves();
 });
 
-function createMoves(array) {
-    console.log("creating moves array...")
-    $("#status").text("Watch Closely...");
-    array.push(colorsArray[(Math.floor(Math.random() * colorsArray.length))]);
-    if (gameArray[count] === gameArray[count - 1]) {
-        gameArray.pop();
-        console.log("preparing to activate light")
-        createMoves(gameArray);
-    }
-    lightUp(gameArray);
-    console.log(gameArray);
+function createMoves() {
+    gameArray.push(colorsArray[Math.floor(Math.random() * 4)]);
+    console.log("gameArray created");
+    showMoves();
 }
-
-function lightUp(array) {
+function showMoves() {
     var i = 0;
-    while (i < array.length) {
-        console.log("cycle " + i + " through light loop");
-        if (array[i] === "green") {
-            i++;
-            $("#green").addClass("active");
-            setTimeout(function () {
-                console.log("waiting to deactivate light");
-                $("#green").removeClass("active");
-            }, 500);
-        } else if (array[i] === "red") {
-            i++;
-            $("#red").addClass("active");
-            setTimeout(function () {
-                console.log("waiting to deactivate light");
-                $("#red").removeClass("active");
-            }, 500);
-        } else if (array[i] === "yellow") {
-            i++;
-            $("#yellow").addClass("active");
-            setTimeout(function () {
-                console.log("waiting to deactivate light");
-                $("#yellow").removeClass("active");
-            }, 500);
-        } else if (array[i] === "blue") {
-            i++;
-            $("#blue").addClass("active");
-            setTimeout(function () {
-                console.log("waiting to deactivate light");
-                $("#blue").removeClass("active");
-            }, 500);
+    var display = setInterval(function () {
+        console.log("sending data to light function");
+        lightUp(gameArray[i]);
+        i++;
+        if (i > gameArray.length) {
+            clearInterval(display);
         }
-    }
-        console.log("waiting 1 second for player turn...");
+    }, 600);
+    $("#status").html("Now it's your turn!");
+}
+function lightUp(string) {
+    if (string === "green") {
+        $("#green").addClass("active");
+        console.log(string+"light on");
         setTimeout(function () {
-            playerTurn();
-
-        }, 1000);
+            $("#green").removeClass("active");
+        }, 250);
+    } else if (string === "red") {
+        $("#red").addClass("active");
+        console.log(string+"light on");
+        setTimeout(function () {
+            $("#red").removeClass("active");
+        }, 250);
+    } else if (string === "yellow") {
+        $("#yellow").addClass("active");
+        console.log(string+"light on");
+        setTimeout(function () {
+            $("#yellow").removeClass("active");
+        }, 250);
+    } else if (string === "blue") {
+        $("#blue").addClass("active");
+        console.log(string+"light on");
+        setTimeout(function () {
+            $("#blue").removeClass("active");
+        }, 250);
+    }
 }
 
-
-    function playerTurn() {
-        console.log("player turn started");
-        playerArray = [];
-        $("#status").text("Now it's your turn!");
-        $("#green").click(function () {
-            playerArray.push("green");
-            console.log("pushed green to player array");
-            if (playerArray.length === gameArray.length) {
-                checkMoves(playerArray, gameArray);
-            }
-        });
-        $("#red").click(function () {
-            playerArray.push("red");
-            console.log("pushed red to player array");
-            if (playerArray.length === gameArray.length) {
-                checkMoves(playerArray, gameArray);
-            }
-        });
-        $("#yellow").click(function () {
-            playerArray.push("yellow");
-            console.log("pushed yellow to player array");
-            if (playerArray.length === gameArray.length) {
-                checkMoves(playerArray, gameArray);
-            }
-        });
-        $("#blue").click(function () {
-            playerArray.push("blue");
-            console.log("pushed blue to player array");
-            if (playerArray.length === gameArray.length) {
-                checkMoves(playerArray, gameArray);
-            }
-        });
+$(".gamecell").click(function(){
+    var button = $(this).attr("id");
+    playerArray.push(button);
+    console.log(button+"added to playerArray");
+    if (playerArray.length === gameArray.length) {
+        $("#status").html("Checking sequence...");
+        checkMoves(playerArray);
     }
+});
 
 
-    function checkMoves(array1, array2) {
-        $("#status").text("Checking your sequence...");
-        console.log("preparing to check moves...");
-        console.log(playerArray);
-        console.log(gameArray);
-        var i = 0;
-        var checkCount = 0;
-        while (i < gameArray.length) {
-            if (array1[i] !== array2[i]) {
-                $("#status").text("Oh no!  Better luck next time!");
-                endGame();
-                break;
-            } else {
-                i++;
-                checkCount++;
-                continue;
-                if (checkCount === gameArray.length) {
-                    $("#status").text("Great Job!  Get ready for the next round!");
-                    break;
-                }
-            }
-        }
-        setTimeout(function () {
-            count++;
-            $("#counter").text("Score: " + count);
-            console.log("waiting 1 second to start next round");
-            createMoves(gameArray)
-        }, 1000);
-
+function checkMoves(array) {
+    console.log("starting move check");
+    var testArray = [];
+    for (var i = 0; i < array.length; i++) {
+        testArray.push(playerArray[i] == gameArray[i]);
     }
+    if (testArray.includes(false)) {
+        console.log("game over-move check complete");
+        $("#status").text("Oh no!  Better luck next time!");
+        endGame();
+    } else {
+        console.log("Start next round-move check complete");
+        count++;
+        $("#status").text("Great Job!  Get ready for the next round!");
+        createMoves();
+    }
+}
 
+var endGame = function () {
+    count = 0;
+    gameArray = [];
+    playerArray = [];
+    $("#counter").text("Score: " + count);
+    setTimeout(function () {
+        $("#status").text("Press Start to play again!");
+    }, 500);
+};
 
-    var endGame = function () {
-        count = 0;
-        gameArray = [];
-        playerArray = [];
-        $("#counter").text("Score: " + count);
-        setTimeout(function () {
-            $("#status").text("Press Start to play again!");
-        }, 500);
-    };
 
 
 
